fed43244fa2cd6fc37ec4b501e75e9deadf43fdd
samuelwaltongallardo <samuelwaltongallardo@gmail.com>
2026-01-12 14:15:20 +0100

    frist

---
+++ app.js
@@ -0,0 +1,21 @@
+let idIterator = 1;
+let numberImages = document.getElementById('number');
+
+let contenidoTabla = document.getElementById('table')
+let nuevaFila = document.createElement('tr')
+nuevaFila.innerHTML = `
+<tr>
+    <td>
+    
+    </td>
+</tr>
+`;
+
+function showingImages(){
+    for (let i = 0; i = numberImages; i++){
+
+    }
+}
+
+
+
