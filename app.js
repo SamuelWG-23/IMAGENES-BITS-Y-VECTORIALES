8b72196ce0d2fe6586dbf645ebb8cf809128bf39
samuelwaltongallardo <samuelwaltongallardo@gmail.com>
2026-01-15 12:20:13 +0100

    almost done

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
 let idIterator = 1;
-let numberImages = document.getElementById('number');
+const selectorValue = document.getElementById('number');
 
-let contenidoTabla = document.getElementById('table')
-let nuevaFila = document.createElement('tr')
-nuevaFila.innerHTML += `
+selectorValue.addEventListener('change', function () {
+  const numImg = parseInt(this.value);
+  showingImages(numImg)
+});
+
+
+function showingImages(numberImages) {
+  let contenidoTabla = document.getElementById('table');
+  contenidoTabla.innerHTML = `
+    <table id="table">
       <tr>
         <th>Image</th>
         <th>ID</th>
         <th>Description</th>
       </tr>
-`;
-
-function showingImages(){
-    for (let i = 0; i = numberImages; i++){
-
-    }
+    </table>`;
+  for (let i = 0; i < numberImages; i++) {
+    let randomIdImage = Math.floor(Math.random() * 200);
+    let nuevaFila = document.createElement('tr');
+    nuevaFila.innerHTML = `
+      <tr>
+        <th><img src="https://picsum.photos/id/${randomIdImage}/200/300" loading="lazy"></img></th>
+        <th>${randomIdImage}</th>
+        <th>Imagen ${i+1 }</th>
+      </tr>
+      `;
+    idIterator++;
+    contenidoTabla.appendChild(nuevaFila);
+  }
 }
+
+document.addEventListener('DOMContentLoaded', () => showingImages(5));
+
+
 
 
 
